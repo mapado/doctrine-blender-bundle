90ad9943d74555bb22a1f114c6c8b7ecda0fe8c4
Julien Deniau <julien.deniau@gmail.com>
2014-09-02 17:54:14 +0200

    add elasticsearch matching

--- src/EntityListener.php
+++ src/EntityListener.php
@@ -2,6 +2,7 @@
 
 namespace Mapado\DoctrineBlenderBundle;
 
+use Doctrine\Common\EventArgs;
 use Doctrine\Common\Persistence\Event\LoadClassMetadataEventArgs;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -34,6 +35,14 @@ class EntityListener
     private $blender;
 
     /**
+     * registeredClasses
+     *
+     * @var array
+     * @access private
+     */
+    private $registeredClasses;
+
+    /**
      * registeredManagers
      *
      * @var array
@@ -54,45 +63,94 @@ class EntityListener
         $this->configuration = $configuration;
 
         $this->blender = new ObjectBlender;
+        $this->registeredClasses = [];
         $this->registeredManagers = [];
     }
 
     /**
      * Registered the postLoad calls for wanted classes
      *
-     * @param LoadClassMetadataEventArgs $event
+     * @param EventArgs $event
      * @access public
      * @return void
      */
-    public function loadClassMetadata(LoadClassMetadataEventArgs $event)
+    public function loadClassMetadata(EventArgs $event)
     {
-        $className = $event->getClassMetadata()->getName();
+        if ($event instanceof LoadClassMetadataEventArgs) {
+            $className = $event->getClassMetadata()->getName();
+            $this->loadExternalAssociationByClassName($className);
+        } elseif (method_exists($event, 'getObjectManager')) {
+            $this->loadExternalAssociationByManager($event->getObjectManager());
+        } else {
+            $msg = '$event must implements `Doctrine\Common\Persistence\Event\LoadClassMetadataEventArgs` ' .
+                ' or have a `getObjectManager` method.';
+            throw new \InvalidArgumentException($msg);
+        }
+    }
 
-        if (!isset($this->registeredManagers[$className])) {
-            foreach ($this->configuration as $config) {
-                if ($className == $config['classname']) {
-                    $sourceObjectManager = $this->container->get($config['source_object_manager']);
+    private function loadExternalAssociationByManager($objectManager)
+    {
+        if (in_array($objectManager, $this->registeredManagers)) {
+            return;
+        }
 
-                    foreach ($config['references'] as $propertyName => $reference) {
-                        $refIdGetter = !empty($reference['reference_id_getter']) ? $reference['reference_id_getter'] : null;
-                        $refSetter = !empty($reference['reference_setter']) ? $reference['reference_setter'] : null;
-                        $externalAssociation = new \Mapado\DoctrineBlender\ExternalAssociation(
-                            $sourceObjectManager,
-                            $className,
-                            $propertyName,
-                            $this->container->get($reference['reference_object_manager']),
-                            $reference['reference_class'],
-                            $refIdGetter,
-                            $refSetter
-                        );
-
-
-                        $this->blender->mapExternalAssociation($externalAssociation);
-                    }
-                }
+        foreach ($this->configuration as $config) {
+            $sourceObjectManager = $this->container->get($config['source_object_manager']);
+            if ($sourceObjectManager === $objectManager) {
+                $this->loadExternalAssociation($config, $sourceObjectManager);
             }
+        }
+        $this->registeredManagers[] = $sourceObjectManager;
+    }
 
-            $this->registeredManagers[$className] = true;
+    /**
+     * loadExternalAssociationByClassName
+     *
+     * @param string $className
+     * @access private
+     * @return void
+     */
+    private function loadExternalAssociationByClassName($className)
+    {
+        if (isset($this->registeredClasses[$className])) {
+            return;
+        }
+
+        foreach ($this->configuration as $config) {
+            if ($className == $config['classname']) {
+                $sourceObjectManager = $this->container->get($config['source_object_manager']);
+                $this->loadExternalAssociation($config, $sourceObjectManager);
+            }
+        }
+
+        $this->registeredClasses[$className] = true;
+    }
+
+    /**
+     * loadExternalAssociation
+     *
+     * @param array $config
+     * @param object $sourceObjectManager
+     * @access private
+     * @return void
+     */
+    private function loadExternalAssociation(array $config, $sourceObjectManager)
+    {
+        foreach ($config['references'] as $propertyName => $reference) {
+            $refIdGetter = !empty($reference['reference_id_getter']) ? $reference['reference_id_getter'] : null;
+            $refSetter = !empty($reference['reference_setter']) ? $reference['reference_setter'] : null;
+            $externalAssociation = new \Mapado\DoctrineBlender\ExternalAssociation(
+                $sourceObjectManager,
+                $config['classname'],
+                $propertyName,
+                $this->container->get($reference['reference_object_manager']),
+                $reference['reference_class'],
+                $refIdGetter,
+                $refSetter
+            );
+
+
+            $this->blender->mapExternalAssociation($externalAssociation);
         }
     }
 }
